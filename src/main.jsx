c15da6fa2166b27b4fcaaa69e2211c66702eadc4
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2023-10-31 14:40:09 +0530

    main

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Flight from './Flight'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Flight/>
+  </React.StrictMode>,
+)
