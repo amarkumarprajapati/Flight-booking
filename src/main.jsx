2c1e0c9e85d496788898db36f9c269ed17f78bf7
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2023-11-15 09:21:32 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Flight from './Flight'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Flight from "./Flight";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Flight/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<Flight />);
